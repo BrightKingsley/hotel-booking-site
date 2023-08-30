fb893bb953152c01f42cb744c2f253ce4d5744ac
BrightKingsley <briggskvngzz@gmail.com>
2023-08-30 08:44:10 +0100

    replaced auth images with smaller sizes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // Listen on all network interfaces
+  },
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
